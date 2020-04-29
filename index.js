0805d5b9f396e2ef109af4abfd70ffe03130ccea
troyhatch12 <troyhatchard@gmail.com>
2020-04-29 17:02:59 -0600

    Nearly working but for download filename

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+const axios = require('axios');
 
 const url = 'https://www.thesuperathleteacademy.com/';
 
@@ -12,143 +12,172 @@ const chromeOptions = {
     executablePath:"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
     headless:false, 
     defaultViewPort: null,
-    slowMo:10};
+    slowMo:25};
 
-(async () => {
-  const browser = await puppeteer.launch(chromeOptions);
-  const page = await browser.newPage();
-  await page.setViewport({width:1500, height:1000});
-  console.log("Connecting to URL...")
-  await page.goto(url, {timeout:60000, waitUntil: 'domcontentloaded'})
-    .then( () => console.log("Connected to ", url))
-    .catch((err) => {
-      console.error("Error connecting to url: ", err);
-      process.exit();
-    });
-  // await page.screenshot({path: 'screenshots/img1.png'})
-  //   .then( () => console.log("Screenshot 1 captured"))
-  //   .catch((err) => {
-  //     console.error("Error capturing screenshot: ", err);
-  //     process.exit();
-  //   });
-  //const button = await page.$(() => document.querySelector('ul.nav > li'));
-  //await button.evaluate( button => button.click());
-  //await page.click();
+try {
 
-  let selector = 'li a';
-  await page.waitFor(selector);
-  await page.evaluate((selector) => document.querySelector(selector).click(), selector)
-    .catch((err) => console.error("Error query Selector: ", err));
-  console.log("Clicked Login");
-  await page.waitFor(5000);
-  // await page.screenshot({path: 'screenshots/img2.png'})
-  //   .then(() => console.log("screenshot 2 captured"));
-  //second page
-  selector = "#user_email";
-  await page.waitFor((selector) => !!document.querySelector(selector), {timeout:120000}, selector)
-    .catch((err) => console.error("Error query Selector: ", err));
-  await page.evaluate((selector, BAEMAIL) => document.querySelector(selector).value = BAEMAIL, selector, BAEMAIL)
-    .catch((err) => console.error("Error query Selector: ", err));
-  await page.waitFor(1000);
-  selector = "#user_password";
-  await page.evaluate((selector, BAPASS) => document.querySelector(selector).value = BAPASS, selector, BAPASS)
-    .catch((err) => console.error("Error query Selector: ", err));
-  // await page.screenshot({path: 'screenshots/img3.png'})
-  //   .then(console.log("Screenshot 3 captured"));
-  selector = ".btn"
-  await page.evaluate((selector) => document.querySelector(selector).click(), selector)
-    .catch((err) => console.error("Error query Selector: line 53", err));
-  await page.waitFor(5000);
-  // await page.screenshot({path: 'screenshots/img4.png'})
-  //   .then(console.log("Screenshot 4 captured"));
-
-  //page 3
-  selector = ".course-box-image-container";
-  await page.evaluate((selector) => document.querySelector(selector).click(), selector)
-    .catch((err) => console.error("Error query Selector: ", err));;
-
-  selector = "ul.section-list li a";
-  await page.waitFor(selector)
-    .catch((err) => console.error("Error wait for: ", err));;
-  let lectureLinks = await page.evaluate((selector) => {
-      let lectureList = document.querySelectorAll(selector);
-      let lectures = [];
-      lectureList.forEach((lec) => {
-        lectures.push(lec.href);
-      });
-      return lectures;
-    }, selector)
-    .catch((err) => {
-      console.error("Error, probably wrong selector: ", err);
-      process.exit();
-    });
-
-
-  for (const [i, link] of lectureLinks.entries()) {
-
-    await page.goto(link, {waitUntil:"networkidle0"})
-      .then(console.log(`Waiting for videos on week ${i}...`))
+  (async () => {
+    const browser = await puppeteer.launch(chromeOptions);
+    const page = await browser.newPage();
+    await page.setViewport({width:1500, height:1000});
+    console.log("Connecting to URL...")
+    await page.goto(url, {timeout:60000, waitUntil: 'domcontentloaded'})
+      .then( () => console.log("Connected to ", url))
       .catch((err) => {
-        console.error("Error with lecture links: ", err);
-        process.exit(0);
-      })
-    
-    await page.evaluate(() => {
-      const pdfElements = document.querySelectorAll('.lecture-attachment-type-pdf_embed');
-      for (const pdfElement of pdfElements) {
-        pdfElement.remove();
-      }
-    });
-
-    // await page.waitFor(10000);
-    // let screenshotName = `screenshots/week${i}.jpeg`
-    // await page.screenshot({path: screenshotName, fullPage:true, quality:10})
-    //   .catch((err) => {
-    //     console.error("Screenshot Error", err);
-    //   });
-    let selector = 'img.w-css-reset';
-    await page.waitFor(selector);
-    const videoLinks = await page.evaluate((selector) => {
-      let videos = document.querySelectorAll(selector);
-      let vidLinks = [];
-      videos.forEach((video) => {
-        let videoUrl = video.src;
-        vidLinks.push(videoUrl);
+        console.error("Error connecting to url: ", err);
+        process.exit();
       });
-      return vidLinks;
-    }, selector);
+    // await page.screenshot({path: 'screenshots/img1.png'})
+    //   .then( () => console.log("Screenshot 1 captured"))
+    //   .catch((err) => {
+    //     console.error("Error capturing screenshot: ", err);
+    //     process.exit();
+    //   });
+    //const button = await page.$(() => document.querySelector('ul.nav > li'));
+    //await button.evaluate( button => button.click());
+    //await page.click();
 
- 
-    console.log(videoLinks);
+    let selector = 'li a';
+    await page.waitFor(selector);
+    await page.evaluate((selector) => document.querySelector(selector).click(), selector)
+      .catch((err) => console.error("Error query Selector: ", err));
+    console.log("Clicked Login");
+    await page.waitFor(5000);
+    // await page.screenshot({path: 'screenshots/img2.png'})
+    //   .then(() => console.log("screenshot 2 captured"));
+    //second page
+    selector = "#user_email";
+    await page.waitFor((selector) => !!document.querySelector(selector), {timeout:120000}, selector)
+      .catch((err) => console.error("Error query Selector: ", err));
+    await page.evaluate((selector, BAEMAIL) => document.querySelector(selector).value = BAEMAIL, selector, BAEMAIL)
+      .catch((err) => console.error("Error query Selector: ", err));
+    await page.waitFor(1000);
+    selector = "#user_password";
+    await page.evaluate((selector, BAPASS) => document.querySelector(selector).value = BAPASS, selector, BAPASS)
+      .catch((err) => console.error("Error query Selector: ", err));
+    // await page.screenshot({path: 'screenshots/img3.png'})
+    //   .then(console.log("Screenshot 3 captured"));
+    selector = ".btn"
+    await page.evaluate((selector) => document.querySelector(selector).click(), selector)
+      .catch((err) => console.error("Error query Selector: line 53", err));
+    await page.waitFor(5000);
+    // await page.screenshot({path: 'screenshots/img4.png'})
+    //   .then(console.log("Screenshot 4 captured"));
+
+    //page 3
+    selector = ".course-box-image-container";
+    await page.evaluate((selector) => document.querySelector(selector).click(), selector)
+      .catch((err) => console.error("Error query Selector: ", err));;
+
+    selector = "ul.section-list li a";
+    await page.waitFor(selector)
+      .catch((err) => console.error("Error wait for: ", err));;
+    let lectureLinks = await page.evaluate((selector) => {
+        let lectureList = document.querySelectorAll(selector);
+        let lectures = [];
+        lectureList.forEach((lec) => {
+          lectures.push(lec.href);
+        });
+        return lectures;
+      }, selector)
+      .catch((err) => {
+        console.error("Error, probably wrong selector: ", err);
+        process.exit();
+      });
+
+    const videosUrls = [];
 
 
-    // const videoElements = await page.$$('video');
-    // for (const videoElement of videoElements) {
-    //   const videoUrl = await videoElement.getProperty('src');
-    //   console.log(videoUrl);
-    //   videoLinks.push(videoUrl);
-    // }
-  }
+    for (const [i, link] of lectureLinks.entries()) {
+
+      await page.goto(link, {waitUntil:"networkidle0"})
+        .then(console.log(`Waiting for videos on week ${i}...`))
+        .catch((err) => {
+          console.error("Error with lecture links: ", err);
+          process.exit(0);
+        })
+      
+      await page.evaluate(() => {
+        const pdfElements = document.querySelectorAll('.lecture-attachment-type-pdf_embed');
+        for (const pdfElement of pdfElements) {
+          pdfElement.remove();
+        }
+      });
+
+      // await page.waitFor(10000);
+      // let screenshotName = `screenshots/week${i}.jpeg`
+      // await page.screenshot({path: screenshotName, fullPage:true, quality:10})
+      //   .catch((err) => {
+      //     console.error("Screenshot Error", err);
+      //   });
+      let selector = '.attachment-wistia-player';
+      await page.waitFor(selector);
+      const jsonIds = await page.evaluate((selector) => {
+        let players = document.querySelectorAll(selector);
+        let links = [];
+        players.forEach((player) => {
+          let linkUrl = player.id.slice(7);
+          links.push(linkUrl);
+        });
+        return links;
+      }, selector);
+
+      jsonLinks = jsonIds.map((jsonId) => {
+        return "http://fast.wistia.com/embed/medias/" + jsonId + ".json";
+      });
+
+      let videoLinks = [];
+
+      for (const [i, jsonLink] of jsonLinks.entries()) {
+        await page.goto(jsonLink);
+        pageContent = await page.evaluate(() => {
+          return JSON.parse(document.querySelector("body").innerText);
+        });
+        videoLinks.push(pageContent.media.assets[2].url); //2 is the index of the mp4 version of the file
+
+      }
+
+      videosUrls.push(videoLinks);
+
+     // break; //for testing. Only want to use one lecture
+    }
+
+    console.log(videosUrls);
+
+    for(const [i, urls] of videosUrls.entries()) {
+      for( const [j, vidUrl] of urls.entries()) {
+        await page.goto(vidUrl);
+        await page.evaluate(() => {
+          const anchor = document.createElement('a');
+          const sourceEl = document.querySelector('video source');
+          const source = sourceEl.getAttribute('src');
+          anchor.setAttribute('href', source);
+          anchor.setAttribute('download', `BALec${i}Vid${j}.mp4`);
+          document.body.appendChild(anchor);
+          anchor.click();
+        });
+      }
+    }
+
+    // videoUrls.forEach(async(urlList) => {
+    //   urlList.forEach(async(url) => {
+    //     console.log(url);
+    //     console.log(typeof(url));
+    //     await page.goto(url);
+    //     await page.waitFor(10000);
+    //   });
+    // });
+
+    console.log('We\'re at the end');
+
+
+    await browser.close();
 
 
 
-  // let lectureAtt = await page.evaluate(() => document.querySelectorAll('div.lecture-attachment'));
-  // console.log(lectureAtt);
+  })();
+} catch (err) {
+  console.error("There was an error", err);
+}
 
-  console.log('We\'re at the end');
 
-
-  await browser.close();
-
-})();
-// axios(url)
-//   .then(respose => {
-//     const html = response.data;
-//     const $ = cheerio.load(html);
-//     const courseSections = ('.course-mainbar > .row');
-//     const courseLinks = [];
-//
-//     courseSections.each(function () {
-//
-//     })
-//   })
